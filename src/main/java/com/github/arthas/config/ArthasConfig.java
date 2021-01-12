9c49a693777068c3fe10ad648df4f0ac3ac418bd
HellcrowAndrey <bakamakafoqq@gmail.com>
2021-01-12 15:17:16 +0200

    update dependency and add conditional on missing bean

--- src/main/java/com/github/arthas/config/ArthasConfig.java
+++ src/main/java/com/github/arthas/config/ArthasConfig.java
@@ -1,6 +1,7 @@
 package com.github.arthas.config;
 
 import com.github.arthas.ArthasBeanPostProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ArthasConfig {
 
     @Bean
+    @ConditionalOnMissingBean(WebClient.class)
     public WebClient webClient() {
         return WebClient.builder().build();
     }
