52c1e6fd77f7dc3503c68bc7226ca18e8216c854
hellcrow <bakamakafoqq@gmail.com>
2023-02-12 13:34:15 +0200

    stable version with reactor load balancer

--- arthas-spring-boot-autoconfiguration/src/main/java/com/github/autoconfiguration/ArthasConfiguration.java
+++ arthas-spring-boot-autoconfiguration/src/main/java/com/github/autoconfiguration/ArthasConfiguration.java
@@ -1,12 +1,11 @@
 package com.github.autoconfiguration;
 
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-//import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Conditional;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,15 +13,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class ArthasConfiguration {
 
-//    @Bean
-//    @Order(value = 0)
-//    @LoadBalanced
-//    @ConditionalOnMissingBean(WebClient.class)
-//    @Conditional(value = ArthasEurekaClientCondition.class)
-//    public WebClient.Builder webClientWithLoadBalanced(ReactorLoadBalancerExchangeFilterFunction lbFunction) {
-//        return WebClient.builder()
-//                .filter(lbFunction);
-//    }
+    @Bean
+    @Order(value = 0)
+    @LoadBalanced
+    @ConditionalOnMissingBean(WebClient.Builder.class)
+    @Conditional(value = ArthasEurekaClientCondition.class)
+    public WebClient.Builder webClientWithLoadBalanced(ApplicationContext context) {
+        return WebClient.builder()
+                .filter(context.getBean(ReactorLoadBalancerExchangeFilterFunction.class));
+    }
 
     @Bean
     @Order(value = 1)
